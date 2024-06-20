42821c455083fcbc22b6ff20ed4e38cc655e5061
NewsBee13 <mstrdzaki@gmail.com>
2024-06-21 04:34:02 +0700

    api update

--- src/app/api/ujian/submit/route.ts
+++ src/app/api/ujian/submit/route.ts
@@ -3,13 +3,6 @@ import prismadb from "@/app/lib/prismadb";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-interface ResponseData {
-  content: string;
-  score: number;
-  questionId: number;
-  attemptId: number;
-}
-
 export const POST = async (req: NextRequest) => {
   const session: any = await getServerSession(authOptions);
   if (!session) {
@@ -26,49 +19,51 @@ export const POST = async (req: NextRequest) => {
 
   try {
     let totalScore = 0;
-    const responseData: ResponseData[] = [];
-    const questionIds = responses.map((response: any) => parseInt(response._id, 10));
 
-    const existingResponses = await prismadb.response.findMany({
-      where: {
-        attemptId: attemptNumber,
-        questionId: { in: questionIds },
-      },
-    });
+    // Menggunakan transaksi Prisma untuk memastikan semua operasi dilakukan dalam satu transaksi
+    await prismadb.$transaction(async (tx) => {
+      const responseData = [];
+      const questionIds = responses.map((response: any) => parseInt(response._id, 10));
 
-    const existingResponseMap = new Map(existingResponses.map((res) => [res.questionId, res]));
-
-    for (const response of responses) {
-      const questionId = parseInt(response._id, 10);
-      if (isNaN(questionId) || existingResponseMap.has(questionId)) continue;
-
-      const question = await prismadb.question.findUnique({
-        where: { id: questionId },
-        include: { Choices: true },
+      const existingResponses = await tx.response.findMany({
+        where: {
+          attemptId: attemptNumber,
+          questionId: { in: questionIds },
+        },
       });
 
-      if (!question) continue;
+      const existingResponseMap = new Map(existingResponses.map((res) => [res.questionId, res]));
 
-      let questionScore = 0;
-      if (question.type === "TKP" && response.response) {
-        const selectedChoice = question.Choices.find(choice => choice.id === parseInt(response.response, 10));
-        questionScore = selectedChoice ? selectedChoice.scoreValue : 0;
-      } else {
-        const isCorrectAnswer = question.Choices.some(choice => choice.isCorrect && choice.id === parseInt(response.response, 10));
-        questionScore = isCorrectAnswer ? 5 : 0;
+      for (const response of responses) {
+        const questionId = parseInt(response._id, 10);
+        if (isNaN(questionId) || existingResponseMap.has(questionId)) continue;
+
+        const question = await tx.question.findUnique({
+          where: { id: questionId },
+          include: { Choices: true },
+        });
+
+        if (!question) continue;
+
+        let questionScore = 0;
+        if (question.type === "TKP" && response.response) {
+          const selectedChoice = question.Choices.find(choice => choice.id === parseInt(response.response, 10));
+          questionScore = selectedChoice ? selectedChoice.scoreValue : 0;
+        } else {
+          const isCorrectAnswer = question.Choices.some(choice => choice.isCorrect && choice.id === parseInt(response.response, 10));
+          questionScore = isCorrectAnswer ? 5 : 0;
+        }
+
+        totalScore += questionScore;
+
+        responseData.push({
+          content: response.response,
+          score: questionScore,
+          questionId: questionId,
+          attemptId: attemptNumber,
+        });
       }
 
-      totalScore += questionScore;
-
-      responseData.push({
-        content: response.response,
-        score: questionScore,
-        questionId: questionId,
-        attemptId: attemptNumber,
-      });
-    }
-
-    await prismadb.$transaction(async (tx) => {
       if (responseData.length > 0) {
         await tx.response.createMany({
           data: responseData,
@@ -82,23 +77,16 @@ export const POST = async (req: NextRequest) => {
       for (const question of allQuestions) {
         const totalResponses = question.responses.length;
 
-        const updateData = question.Choices.map((choice) => {
+        for (const choice of question.Choices) {
           const choiceResponses = question.responses.filter(
             response => response.content === choice.id.toString()
           ).length;
 
           const percentage = totalResponses ? (choiceResponses / totalResponses) * 100 : 0;
 
-          return {
-            id: choice.id,
-            percentage: percentage,
-          };
-        });
-
-        for (const data of updateData) {
           await tx.choice.update({
-            where: { id: data.id },
-            data: { percentage: data.percentage },
+            where: { id: choice.id },
+            data: { percentage },
           });
         }
       }
